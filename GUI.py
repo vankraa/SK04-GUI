9bbb4290fc75d3e5de0dc42cb740e08ec9daee4f
shanf2 <shanf2@mcmaster.ca>
2020-10-09 20:49:10 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -9,15 +9,21 @@ canvas = tk.Canvas(root, height = HEIGHT, width = WIDTH)
 canvas.pack()
 
 frame = tk.Frame(root, bg='#80c1ff') # 80c1ff hex for blue hex
-frame.place(relx=0.1,rely=0.1,relwidth=0.8,relheight= 0.8)
+frame.place(relx = 0.1, rely = 0.1,relwidth = 0.8,relheight = 0.8)
 
-button = tk.Button(frame, text = "Text button")
-button.pack()
+label = tk.Label(frame, text="Username: ", bg='yellow')
+label.place(relx = 0.44, rely = 0.4, relwidth = 0.13, relheight = 0.05)
 
-label = tk.Label(frame, text="This is a label", bg='yellow')
-label.pack()
+label = tk.Label(frame, text="Password:  ", bg='yellow')
+label.place(relx = 0.44, rely = 0.46, relwidth = 0.13, relheight = 0.05)
 
 entry = tk.Entry(frame, bg='green')
-entry.pack()
+entry.place(relx = 0.58, rely = 0.4, relwidth = 0.15, relheight = 0.05)
+
+entry = tk.Entry(frame, bg='green')
+entry.place(relx = 0.58, rely = 0.46, relwidth = 0.15, relheight = 0.05)
+
+button = tk.Button(frame, text = "Enter")
+button.place(relx = 0.61, rely = 0.52, relwidth = 0.12, relheight = 0.05)
 
 root.mainloop() 
